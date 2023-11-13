e2f08db2a0bee3b7df94253d0a00fbe7b07a75f3
bruno <brunocanini1994@gmail.com>
2023-11-13 16:26:56 +0100

    feat route posts:: /,show,create,download

--- controllers/posts.js
+++ controllers/posts.js
@@ -2,18 +2,35 @@ const express = require("express");
 const fs = require("fs");
 const path = require("path");
 
+const postsDb = require("../db");
+
 function index (req, res) {
 
     res.format({
       text: () => {
-        res.type("text").send("Hello World!");
+        res.type("text").send("POSTS");
       },
       html: () => {
-        res.type("html").send("<h1>Hello World!</h1>");
+       const html = ["<h1>POSTS</h1>"];
+    
+       html.push("ul");
+
+       for ( post of postsDb ) {
+        html.push(`<li>
+            <h3>${post.title}<h3/>
+            </li>`)
+       };
+
+       html.push("</ul>");
+
+       res.send(html.join(""));
+
+
+
       },
       json: () => {
         res.type("json").send({
-          message: "Hello World!",
+          message: "POSTS",
         });
       },
       default: () => {
@@ -22,6 +39,49 @@ function index (req, res) {
     })
   }
 
+function show (req, res) {
+
+    const postSlug = req.params.slug
+    const post = postsDb.find(post => post.slug == postSlug)
+
+    if(!post){
+        res.status(404).send("post non trovato");
+    }
+
+    res.json(post);
+  }
+
+function create (req, res) {
+    res.format({
+      html: () => {
+        res.type("html").send("<h1>Creazione nuovo post</h1>");
+
+      },
+      default: () => {
+        res.status(406).send("Not Acceptable");
+      },
+    })
+  }
+
+function download (req, res) {
+
+    const postSlug = req.params.slug
+    const post = postsDb.find(post => post.slug == postSlug)
+    const filePath = path.resolve(__dirname, "..", "public", "imgs", "posts", post.image);
+
+    if(!post){
+        res.status(404).send("post non trovato");
+    }
+
+    res.download(filePath);
+
+  }
+
+
+
   module.exports = {
     index,
+    show,
+    create,
+    download
   }
